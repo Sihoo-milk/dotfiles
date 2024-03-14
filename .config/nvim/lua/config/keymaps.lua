079d0ca5b8c5b1e76966c8be44ec07c982de1c2f
tomohiro.maeno <>
2024-03-14 23:21:40 +0700

    add: Add keymap settings for LazyVim.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,25 +2,43 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.api.nvim_set_keymap('i', 'jj', '<ESC>', { noremap = true, silent = true })
+-- Bind jj to <Esc> in insert mode.
+vim.api.nvim_set_keymap("i", "jj", "<ESC>", { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap('n', '<Leader>w', ':w<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<Leader>q', ':q<CR>', { noremap = true, silent = true })
+-- Save and close the file.
+vim.api.nvim_set_keymap("n", "<Leader>w", ":w<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>q", ":q<CR>", { noremap = true, silent = true })
 
 -- Redo
-vim.api.nvim_set_keymap('n', 'U', '<C-r>', { noremap = true })
+vim.api.nvim_set_keymap("n", "U", "<C-r>", { noremap = true })
 
--- Select all
-vim.api.nvim_set_keymap('n', '<Leader>a', 'ggVG', { noremap = true })
+-- Select all.
+vim.api.nvim_set_keymap("n", "<Leader>a", "ggVG", { noremap = true })
 
--- Move up/down by display line
-vim.api.nvim_set_keymap('n', 'k', 'gk', { noremap = true })
-vim.api.nvim_set_keymap('n', 'j', 'gj', { noremap = true })
+-- Copy from the cursor position to the end of the line.
+vim.api.nvim_set_keymap("n", "Y", "y$", { noremap = true })
+
+-- Move up/down by display line.
+vim.api.nvim_set_keymap("n", "k", "gk", { noremap = true })
+vim.api.nvim_set_keymap("n", "j", "gj", { noremap = true })
 
 -- Scroll
-vim.api.nvim_set_keymap('n', 'zk', 'zt', { noremap = true })
-vim.api.nvim_set_keymap('n', 'zj', 'zb', { noremap = true })
+vim.api.nvim_set_keymap("n", "zk", "zt", { noremap = true })
+vim.api.nvim_set_keymap("n", "zj", "zb", { noremap = true })
+
+-- Remove search highlight.
+vim.api.nvim_set_keymap("n", "<C-n>", ":noh<CR>", { noremap = true })
+
+-- Split window
+vim.api.nvim_set_keymap("n", "ss", "<C-w>s", { noremap = true })
+vim.api.nvim_set_keymap("n", "sv", "<C-w>v", { noremap = true })
+
+-- Move the cursor between windows.
+vim.api.nvim_set_keymap("n", "sk", "<C-w>k", { noremap = true })
+vim.api.nvim_set_keymap("n", "sj", "<C-w>j", { noremap = true })
+vim.api.nvim_set_keymap("n", "sh", "<C-w>h", { noremap = true })
+vim.api.nvim_set_keymap("n", "sl", "<C-w>l", { noremap = true })
 
 -- Tab
-vim.api.nvim_set_keymap('n', 'gh', 'gT', { noremap = true })
-vim.api.nvim_set_keymap('n', 'gl', 'gt', { noremap = true })
+vim.api.nvim_set_keymap("n", "gh", "gT", { noremap = true })
+vim.api.nvim_set_keymap("n", "gl", "gt", { noremap = true })
